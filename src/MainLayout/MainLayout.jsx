a25ecd068f8762c860cced205f6acd083d7a0387
chinmoy10 <chinmoypodder22@gmail.com>
2026-04-27 03:05:37 +0600

    running the project mealdb

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import "../CSS/style.css"
 
 const MainLayout = () => {
+    const navigation = useNavigation();
     return (
         <>
             <Navbar></Navbar>
-            <div className='w-10/12 mx-auto'>
-                <Outlet></Outlet>
-            </div>
+            {
+                navigation.state === "loading" ?
+                    <p className='flex justify-center items-center h-[800px]'>loading...</p> :
+                    <div className='w-10/12 mx-auto'>
+                        <Outlet></Outlet>
+                    </div>
+            }
         </>
     );
 };
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bannerImg from "../assets/banner.jpg"
+import { Link } from 'react-router-dom';
 const Banner = () => {
     return (
         <>
@@ -8,7 +9,12 @@ const Banner = () => {
                 <div className='bg-slate-950/55 w-full h-full'>
                     <div className='flex flex-col gap-4 items-center justify-center text-center h-full'>
                         <h1 className='text-8xl font-semibold text-white'>Best Food Center</h1>
-                        <p className='text-2xl font-light text-gray-200'>There have different types of food items which all are so great and delicious. <br/> So, please come here and select your lovely Yummy Foods.</p>
+                        <p className='text-2xl font-light text-gray-200'>There have different types of food items which all are so great and delicious. <br /> So, please come here and select your lovely Yummy Foods.</p>
+                        <div>
+                            <Link to="/items" >
+                                <button className='btn bg-transparent border-2 border-orange-300 text-white text-xl font-semibold hover:bg-orange-300 hover:border-white hover:text-cyan-950 duration-1000'>All Items</button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
